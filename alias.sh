4f937ebabfdaccea1a44b9d175ccc27a29c6d66c
gcallah <gcallah@mac.com>
2022-09-12 20:38:28 -0400

    Changed txt alias.

--- alias.sh
+++ alias.sh
@@ -62,7 +62,7 @@ alias seng="cd $GDIR/SETemplate"
 alias soc="cd $GDIR/socnet"
 alias tdev="cd $CLASS_DIR/OnlineDevops"
 alias tech="cd $GDIR/TechManagement"
-alias txt="cd $CLASSDIR/TextGame"
+alias txt="cd $CLASS_DIR/TextGame"
 alias utl="cd $GDIR/utils"
 alias work="cd $WRITING_DIR/work"
 
